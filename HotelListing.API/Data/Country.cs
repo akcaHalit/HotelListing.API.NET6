2c4a5c59b4c929687471647a894c649436804305
halit akca <halitakca_70@hotmail.com>
2022-11-27 19:15:34 +0300

    Added Entity Framework and Created DataBase

---
+++ HotelListing.API/Data/Country.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelListing.API.Data
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string shortName { get; set; }
+
+        //one to many || a country may have many hotels.
+
+        public virtual IList<Hotel> Hotels { get; set; }
+        
+    }
+}
---
+++ HotelListing.API/Data/Hotel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListing.API.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }     //Primary Key
+
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public double Rating { get; set; }
+
+        [ForeignKey (nameof(CountryId))]    // [ForeignKey("CountryId")] DO NOT WARN YOU! so||USE the NAMEOF||
+        public int CountryId { get; set; }  //Foreign Key
+        public Country Country { get; set; }
+
+    }
+}
