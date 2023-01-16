407a48fdefd5408cc4718d9f816a1c5d7a59d837
halit akca <halitakca_70@hotmail.com>
2023-01-16 19:23:32 +0300

    REST API Development Best Practices. Automapper and Repositories

---
+++ HotelListing.API/Repository/CountriesRepository.cs
@@ -0,0 +1,25 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Repository
+{
+    public class CountriesRepository : GenericRepository<Country>, ICountriesRepository
+    {
+        // we have copied the context and getdetails part from the countriescontroller for get method
+
+        private readonly HotelListingDbContext _context;
+
+        public CountriesRepository(HotelListingDbContext context) : base(context)
+        {
+            this._context = context;
+        }
+
+        
+        public async Task<Country> GetDetails(int id)
+        {
+            return await _context.Countries.Include(q => q.Hotels)
+             .FirstOrDefaultAsync(q => q.Id == id);
+        }
+    }
+}
